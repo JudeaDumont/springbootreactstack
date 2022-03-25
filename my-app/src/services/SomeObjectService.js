c48fa74f97a3586e3ef56c43d557ebb26a1e5463
Judea <dumontjudea2@gmail.com>
2022-03-25 03:08:43 -0600

    Fix cross origin problem and make the request more manageable

--- my-app/src/services/SomeObjectService.js
+++ my-app/src/services/SomeObjectService.js
@@ -1,12 +1,17 @@
 import axios from 'axios'
 
 const API_URL = 'http://localhost:8080'
-const SOME_OBJECT_NAME_API_POINT = `/getmeobjects/`
+const SOME_OBJECT_NAME_API_POINT = `getmeobjects`
 
 class SomeObjectService {
 
     retrieveAllSomeObjects(name) {
-        return axios.get(`${API_URL}/${name}/${SOME_OBJECT_NAME_API_POINT}`);
+        console.log(`${API_URL}/${name}/${SOME_OBJECT_NAME_API_POINT}`);
+
+        return axios({
+            method: 'get',
+            url: `${API_URL}/${name}/${SOME_OBJECT_NAME_API_POINT}`
+        });
     }
 }
 
--- src/main/java/com/example/demo/controllers/SomeObjectRestController.java
+++ src/main/java/com/example/demo/controllers/SomeObjectRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.models.SomeObject;
 import com.example.demo.services.SomeObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class SomeObjectRestController {
     @Autowired
     private SomeObjectService someObjectService;
 
+    @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
     @GetMapping("/{name}/getmeobjects")
     public List<SomeObject> getAllCourses(@PathVariable String name) {
         return someObjectService.findAll();
