eda2097eaada609dc8eda5c82610a1abd4631a78
Judea <dumontjudea2@gmail.com>
2022-03-25 01:38:57 -0600

    Removing the template header from the react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,20 +5,6 @@ import SomeObjectApp from './component/SomeObjectApp';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          The names Deez
-        </a>
-      </header>
       <div className="container">
         <SomeObjectApp />
       </div>
