86298847134ea7cdce6c5b6fc68ce4e60d2b25fb
Judea <dumontjudea2@gmail.com>
2022-03-25 23:48:11 -0600

    Create an in memory database (that can be converted to a file based database) and seed with random data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE chair (
+    id int COMMENT 'creamed',
+    name varchar(255) COMMENT 'corn'
+);
+
+INSERT INTO chair (id, name) VALUES (1, CAST('Can' as varchar(255)));
+INSERT INTO chair (id, name) VALUES (2, CAST('I' as varchar(255)));
+INSERT INTO chair (id, name) VALUES (3, CAST('Have' as varchar(255)));
+INSERT INTO chair (id, name) VALUES (4, CAST('A' as varchar(255)));
+INSERT INTO chair (id, name) VALUES (5, CAST('Hotdog' as varchar(255)));
+INSERT INTO chair (id, name) VALUES (6, CAST('Boss' as varchar(255)));
