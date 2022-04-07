b1fb9bcd0b36a1f4590f1b5b95bbbbbdf96255c0
Judea <dumontjudea2@gmail.com>
2022-04-07 06:59:48 -0600

    Conditional rendering example and nested children
    commented out components unrelated to current workflow then supressed the warning for unused imports for the file

--- my-app/src/component/ObjectApp.jsx
+++ my-app/src/component/ObjectApp.jsx
@@ -1,23 +1,28 @@
+/* eslint-disable no-unused-vars */
+// noinspection ES6UnusedImports
+
 import React, { Component } from 'react';
 import ObjectListComponent from "./ObjectListComponent";
 import ObjectListByNameComponent from "./ObjectListByNameComponent";
 import ObjectSaveComponent from "./ObjectSaveComponent";
 import ObjectDeleteComponent from "./ObjectDeleteComponent";
 import IntroToLogic from "../FollowReactCrashCourse/introductiontologicincomponent";
+import MyParentComponent from "../FollowReactCrashCourse/ParentChildPropertiesExample";
 
 class ObjectApp extends Component {
     render() {
         return (
             <div>
-                <IntroToLogic/>
-                <ObjectSaveComponent/>
-                <ObjectDeleteComponent/>
-                <ObjectListComponent
-                    title={"List Of Objects!"}
-                />
-                <ObjectListByNameComponent
-                    title={"List Of Objects By Name!"}
-                />
+                <MyParentComponent/>
+                {/*<IntroToLogic/>*/}
+                {/*<ObjectSaveComponent/>*/}
+                {/*<ObjectDeleteComponent/>*/}
+                {/*<ObjectListComponent*/}
+                {/*    title={"List Of Objects!"}*/}
+                {/*/>*/}
+                {/*<ObjectListByNameComponent*/}
+                {/*    title={"List Of Objects By Name!"} //this can be refactored to where the list component is configurable to be a ListByNameComponent*/}
+                {/*/>*/}
             </div>
         )
     }
