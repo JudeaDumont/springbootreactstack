a93e28b9144465f4f1f46ab9258d4360c75d13b4
Judea <dumontjudea2@gmail.com>
2022-04-07 03:25:30 -0600

    Following the crash course and adding a component for saying hello with some logic embedded

--- my-app/src/component/ObjectApp.jsx
+++ my-app/src/component/ObjectApp.jsx
@@ -3,11 +3,13 @@ import ObjectListComponent from "./ObjectListComponent";
 import ObjectListByNameComponent from "./ObjectListByNameComponent";
 import ObjectSaveComponent from "./ObjectSaveComponent";
 import ObjectDeleteComponent from "./ObjectDeleteComponent";
+import IntroToLogic from "../FollowReactCrashCourse/introductiontologicincomponent";
 
 class ObjectApp extends Component {
     render() {
         return (
             <div>
+                <IntroToLogic/>
                 <h1>!!!!!Some Object Application!!!!!</h1>
                 <h1>!!!!!WOOHOO!!!!!</h1>
                 <ObjectSaveComponent/>
