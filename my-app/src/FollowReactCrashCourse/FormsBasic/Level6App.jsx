65a2b0d8045cdf1550a6a9d22b06bd9097c5f43d
Judea <dumontjudea2@gmail.com>
2022-04-12 09:02:49 -0600

    Forgot about password confirmation

--- my-app/src/FollowReactCrashCourse/FormsBasic/Level6App.jsx
+++ my-app/src/FollowReactCrashCourse/FormsBasic/Level6App.jsx
@@ -4,6 +4,7 @@ export default function Level6App() {
     const [formData, setFormData] = React.useState({
         email: "",
         password: "",
+        passwordconfirmation: "",
         newsletter: true,
     })
 
@@ -19,8 +20,12 @@ export default function Level6App() {
 
     function handleSubmit(event) {
         event.preventDefault()
-        if (formData.newsletter) {
-            console.log("Thanks for subscribing to our newsletter!")
+
+        if (formData.passwordconfirmation === formData.password) {
+            console.log("Sign up successful!")
+            if (formData.newsletter) {
+                console.log("Thanks for subscribing to our newsletter!")
+            }
         }
     }
 
@@ -40,7 +45,15 @@ export default function Level6App() {
                     placeholder={"Password"}
                     onChange={handleChange}
                     name={"password"}
-                    value={formData.state}
+                    value={formData.password}
+                />
+                <br/>
+                <input
+                    type={"password"}
+                    placeholder={"Password Confirmation"}
+                    onChange={handleChange}
+                    name={"passwordconfirmation"}
+                    value={formData.passwordconfirmation}
                 />
                 <br/>
                 <br/>
@@ -60,5 +73,12 @@ export default function Level6App() {
 }
 
 /*
+buttons inside fomr act as submit already, when you click submit button it runs the onSubmit event handler which is assigned the handleSubmit function already.
+
+then you want to prevent default so that the submission doesn't add all of the form data into the top of the page and
+refresh the page and do a bunch of other things that are useless for us since we are not using antiquated web pages.
+
+password and confirmation match each other then we say ":successfully signed up."
+
 
  */
