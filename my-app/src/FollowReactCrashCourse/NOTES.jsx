9b174dadfa541024b0bb003692e896be51463431
Judea <dumontjudea2@gmail.com>
2022-04-17 14:52:43 -0600

    Ending notes for the crash course

--- my-app/src/FollowReactCrashCourse/NOTES.jsx
+++ my-app/src/FollowReactCrashCourse/NOTES.jsx
@@ -1,1 +1,16 @@
+//Covered an insane amount of topics during this react crash course
 
+/*
+Event listeners are a key component of any interactive web app, crucial spring board to learn, onClick, onSubmit
+State, including the state of components and their side effects throughout the rendering cycle
+Conditional rendering and the ways you can implement it in your code
+Forms in react and how react expedites the process of input usage
+Side effects and how we can handle them, robust introduction to the react library,
+    Reacts provisioning of tools that allow us to sync our interactive web pages with outside systems
+
+Marks the completion of topics we will be learning in this course
+
+Next section is dedicated to practicing everything that we have learned
+
+
+ */
