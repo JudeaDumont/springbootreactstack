68ef8eccbd4710c695b20e885630d923a72395b5
Judea <dumontjudea2@gmail.com>
2022-06-28 01:08:12 -0600

    Made sure that test ordering is working, tested delete method tests getting a deleted object
    want to do an integration test using this junit thing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,17 +2,21 @@ package com.example.demo;
 
 import com.example.demo.database.DatabaseConnecter;
 import com.example.demo.models.SomeObject;
-import com.example.demo.services.SomeObjectService;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DemoApplicationTests {
 
 	@Test
-	void SaveObject() {
+	@Order(1)
+	void saveObject1() {
 		SomeObject so = new SomeObject(12L, "Man");
 		Session session = DatabaseConnecter.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
@@ -21,16 +25,38 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	void RunQuery() {
+	@Order(2)
+	void runQuery() {
 		DatabaseConnecter.getSessionFactory().openSession().createQuery("FROM SomeObject").list();
 	}
 
 	@Test
-	void saveObject() {
+	@Order(3)
+	void saveObject2() {
 		SomeObject so = new SomeObject(15L, "DEEZ");
 		Session session = DatabaseConnecter.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
 		session.save(so);
 		tx.commit();
+	}
+
+	@Test
+	@Order(4)
+	void deleteObject() {
+		SomeObject so = new SomeObject(15L, "DEEZ");
+		Session session = DatabaseConnecter.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		session.delete(so);
+		tx.commit();
+	}
+
+	@Test
+	@Order(5)
+	void getDeletedObject() {
+		Session session = DatabaseConnecter.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		SomeObject shouldBeNull = session.get(SomeObject.class, 15L);
+		tx.commit();
+		assert shouldBeNull == null;
 	}
 }
